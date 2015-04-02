468004ca7cb09028170eab64a4f53af921c237ac
Skyler Hawthorne <skylerhawthorne@gmail.com>
2015-04-02 12:53:30 -0700

    Add basic document downloading

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -1,8 +1,37 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /*
  * Scrapes data from a web site.
  *
  * @author dead10ck, @date 4/2/15 11:58 AM
  */
 public class Scraper {
+    private static final String BASE_URI =
+        "http://www.shodor.org/interactivate/activities/";
 
+    public enum Endpoint {
+        BY_SUBJECT("bySubject/"),
+        BY_AUDIENCE("byAudience/");
+
+        private String endpointString;
+
+        Endpoint(String str) {
+            this.endpointString = str;
+        }
+
+        /**
+         * @return the endpointString
+         */
+        public String toString() {
+            return endpointString;
+        }
+    }
+
+    public static Document getDocument(Endpoint ep) throws IOException {
+        String url = BASE_URI + ep.toString();
+        return Jsoup.connect(url).get();
+    }
 }
--- src/test/java/ScraperTest.java
+++ src/test/java/ScraperTest.java
@@ -1,4 +1,11 @@
+import java.io.IOException;
+
+import org.jsoup.nodes.Document;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.Assert.*;
 
 /*
@@ -7,6 +14,26 @@ import static org.junit.Assert.*;
  * @author dead10ck, @date 4/2/15 11:58 AM
  */
 public class ScraperTest {
+    @SuppressWarnings("unused")
+    private static Logger logger = LoggerFactory.getLogger(ScraperTest.class);
+    private static Document bySubjectDoc;
+    private static Document byAudienceDoc;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        bySubjectDoc = Scraper.getDocument(Scraper.Endpoint.BY_SUBJECT);
+        //logger.info(String.format("Document text:\n%s\n", bySubjectDoc.outerHtml()));
+        assertNotNull(bySubjectDoc);
+        assertTrue(0 < bySubjectDoc.childNodes().size());
+
+        byAudienceDoc = Scraper.getDocument(Scraper.Endpoint.BY_AUDIENCE);
+        //logger.info(String.format("Document text:\n%s\n", byAudienceDoc.outerHtml()));
+        assertNotNull(byAudienceDoc);
+        assertTrue(0 < byAudienceDoc.childNodes().size());
+    }
+
     @Test
-    public void testSomeLibraryMethod() {}
+    public void testDownloadWebDocument() throws IOException {
+
+    }
 }
