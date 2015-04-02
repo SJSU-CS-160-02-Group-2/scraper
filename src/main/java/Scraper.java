358989e13ef542c10c42c0d74ba91011ab051298
Skyler Hawthorne <skylerhawthorne@gmail.com>
2015-04-02 12:06:39 -0700

    Init

---
+++ src/main/java/Scraper.java
@@ -0,0 +1,8 @@
+/*
+ * Scrapes data from a web site.
+ *
+ * @author dead10ck, @date 4/2/15 11:58 AM
+ */
+public class Scraper {
+
+}
---
+++ src/test/java/ScraperTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+ * Scraper tests.
+ *
+ * @author dead10ck, @date 4/2/15 11:58 AM
+ */
+public class ScraperTest {
+    @Test
+    public void testSomeLibraryMethod() {}
+}
