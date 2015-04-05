f8d770c8643d739b72bba1a9a2ef0717ad9345f1
Skyler Hawthorne <skylerhawthorne@gmail.com>
2015-04-05 15:07:26 -0700

    Add a small main class and instructions

---
+++ README.md
@@ -0,0 +1,7 @@
+To run, clone this repo, cd into it, and run this at the command line:
+
+```sh
+$ ./gradlew run
+```
+
+Or, if you're on Windows, use `gradlew.bat`, instead of `gradlew`.
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+import org.jsoup.nodes.Document;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        ShodorScraper scraper = new ShodorScraper();
+        Document bySubjectDoc = scraper.bySubject();
+        Document byAudienceDoc = scraper.byAudience();
+        Set<SiteEntry> sites = scraper.scrape(bySubjectDoc, byAudienceDoc);
+
+        for (SiteEntry site : sites) {
+            System.out.printf("%s\n\n", site.toString());
+        }
+    }
+}
